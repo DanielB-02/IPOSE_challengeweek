d071023e458ac2396f47303dcbd40bafa64d2b57
AnonymousWebDev <100128021+AnonymousWebDev@users.noreply.github.com>
2023-04-03 15:39:29 +0200

    game

--- Docenten_vs_Studenten/src/main/java/org/hsleiden/challengeweek/docenten_vs_studenten/Level1.java
+++ Docenten_vs_Studenten/src/main/java/org/hsleiden/challengeweek/docenten_vs_studenten/Level1.java
@@ -1,22 +1,21 @@
 package org.hsleiden.challengeweek.docenten_vs_studenten;
 
-import com.almasb.fxgl.app.CursorInfo;
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.Entity;
-import javafx.scene.input.KeyCode;
+import javafx.scene.control.Button;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
-public class Game extends GameApplication {
+
+public class Level1 extends GameApplication {
     @Override
     protected void initSettings(GameSettings Settings) {
         Settings.setTitle("Teachers vs Students");
         Settings.setFullScreenAllowed(true);
         Settings.setFullScreenFromStart(true);
     }
-
     private Entity player;
 
     @Override
@@ -26,24 +25,5 @@ public class Game extends GameApplication {
                 .view(new Rectangle(30, 30, Color.RED))
                 .buildAndAttach();
     }
-
-    @Override
-    protected void initInput(){
-        FXGL.onKey(KeyCode.D, () ->{
-            player.translateX(5);
-        });
-        FXGL.onKey(KeyCode.A, () ->{
-            player.translateX(-5);
-        });
-        FXGL.onKey(KeyCode.W, () ->{
-            player.translateY(-5);
-        });
-        FXGL.onKey(KeyCode.S, () ->{
-            player.translateY(5);
-        });
-    }
-
-    public static void main(String[] args) {
-        launch(args);
-    }
 }
+
