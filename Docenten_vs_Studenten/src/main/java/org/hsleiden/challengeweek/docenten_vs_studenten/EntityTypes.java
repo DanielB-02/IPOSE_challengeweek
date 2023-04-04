d0f0473948da397f3938572a251a4b0b2988755a
terra-ursus <daniel.d.bor@gmail.com>
2023-04-04 09:57:53 +0200

    Added initPhysics collisions

---
+++ Docenten_vs_Studenten/src/main/java/org/hsleiden/challengeweek/docenten_vs_studenten/EntityTypes.java
@@ -0,0 +1,5 @@
+package org.hsleiden.challengeweek.docenten_vs_studenten;
+
+public enum EntityTypes {
+    PLAYER, PLANT, ERWT
+}
--- Docenten_vs_Studenten/src/main/java/org/hsleiden/challengeweek/docenten_vs_studenten/Game.java
+++ Docenten_vs_Studenten/src/main/java/org/hsleiden/challengeweek/docenten_vs_studenten/Game.java
@@ -6,6 +6,8 @@ import com.almasb.fxgl.app.GameSettings;
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.entity.components.BoundingBoxComponent;
+import com.almasb.fxgl.entity.components.CollidableComponent;
+import com.almasb.fxgl.physics.CollisionHandler;
 import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -24,6 +26,8 @@ public class Game extends GameApplication {
     }
 
     private Entity player;
+    private Entity plant;
+
     private Entity erwt;
 
     @Override
@@ -33,10 +37,20 @@ public class Game extends GameApplication {
         player = FXGL.entityBuilder()
                 .at(400, 200)
                 .viewWithBBox(new Rectangle(30, 30, Color.RED))
+                .with(new CollidableComponent(true))
+                .type(EntityTypes.PLAYER)
+                .buildAndAttach();
+        plant = FXGL.entityBuilder()
+                .at(155,155)
+                .viewWithBBox(new Rectangle(30, 30, Color.YELLOW))
+                .with(new CollidableComponent(true))
+                .type(EntityTypes.PLANT)
                 .buildAndAttach();
         erwt = FXGL.entityBuilder()
                 .at(100, 200)
                 .viewWithBBox(new Rectangle(20, 20, Color.BROWN))
+                .with(new CollidableComponent(true))
+                .type(EntityTypes.ERWT)
                 .buildAndAttach();
     }
 
@@ -57,14 +71,24 @@ public class Game extends GameApplication {
     }
 
     @Override
+    protected void initPhysics() {
+        FXGL.getPhysicsWorld().addCollisionHandler(new CollisionHandler(EntityTypes.PLAYER, EntityTypes.ERWT) {
+            @Override
+            protected void onCollision(Entity player, Entity erwt) {
+                System.out.println("hallo");
+                player.removeFromWorld();
+            }
+        });
+    }
+    @Override
     protected void onUpdate(double tpf) {
         erwt.translateX(5);
-        if (erwt.isColliding(player)) {
-            FXGL.entityBuilder()
-                    .at(erwt.getX(), erwt.getY())
-                    .view(new Circle(80, Color.RED))
-                    .buildAndAttach();
-        }
+//        if (erwt.isColliding(player)) {
+//            FXGL.entityBuilder()
+//                    .at(erwt.getX(), erwt.getY())
+//                    .view(new Circle(80, Color.RED))
+//                    .buildAndAttach();
+//        }
     }
 
     public static void main(String[] args) {
