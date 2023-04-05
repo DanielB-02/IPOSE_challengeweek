2d6e9551d585040f0d67bb109ad43124b4025445
terra-ursus <daniel.d.bor@gmail.com>
2023-04-05 23:09:16 +0200

    Added random peas and pea list instead of dictionary

--- Docenten_vs_Studenten/src/main/java/org/hsleiden/challengeweek/docenten_vs_studenten/demo/PlantsApp.java
+++ Docenten_vs_Studenten/src/main/java/org/hsleiden/challengeweek/docenten_vs_studenten/demo/PlantsApp.java
@@ -1,44 +1,40 @@
-package com.example.demo;
+package org.hsleiden.challengeweek.docenten_vs_studenten.demo;
 
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.Entity;
-import com.almasb.fxgl.entity.components.CollidableComponent;
-import com.almasb.fxgl.input.UserAction;
 import com.almasb.fxgl.physics.CollisionHandler;
 import javafx.scene.control.Button;
 import javafx.scene.input.KeyCode;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
+import org.hsleiden.challengeweek.docenten_vs_studenten.EntityTypes;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
-import static com.almasb.fxgl.dsl.FXGL.*;
-import static com.almasb.fxgl.dsl.FXGLForKtKt.texture;
+import static com.almasb.fxgl.dsl.FXGL.getGameWorld;
+import static com.almasb.fxgl.dsl.FXGL.setLevelFromMap;
 
 public class PlantsApp extends GameApplication {
 
+    public static final int WIDTH_BOARD = 16 * 80;
+    public static final int HEIGHT_BOARD = 8 * 80;
     private Entity player1;
     private Entity player2;
     private boolean isPlayer1Active = true;
     boolean userMadeChoice = false;
     private Entity plant1;
     private Entity plant2;
-    private ArrayList<Entity> plants = new ArrayList<>();
+    private List<Entity> plants = new ArrayList<>();
 
-    //    private ArrayList<Entity> erwten = new ArrayList<>();
-    HashMap<Entity, Entity> plantErwtMap = new HashMap<>();
-
-    private Entity erwt;
+    private List<Entity> erwten = new ArrayList<>();
 
     @Override
     protected void initSettings(GameSettings gameSettings) {
-        gameSettings.setWidth(16 * 80);
-        gameSettings.setHeight(8 * 80);
+        gameSettings.setWidth(WIDTH_BOARD);
+        gameSettings.setHeight(HEIGHT_BOARD);
         /*gameSettings.setFullScreenAllowed(true);
         gameSettings.setFullScreenFromStart(true);*/
     }
@@ -64,7 +60,7 @@ public class PlantsApp extends GameApplication {
             initElements();
             player1.removeFromWorld();
             FXGL.getGameScene().removeUINode(player1Button);
-            FXGL.getGameScene().r emoveUINode(player2Button);
+            FXGL.getGameScene().removeUINode(player2Button);
         });
 
         // add buttons to game scene
@@ -80,64 +76,34 @@ public class PlantsApp extends GameApplication {
 
     private void initElements() {
         player1 = getGameWorld().spawn("player1", 700, 200);
+        player2 = getGameWorld().spawn("player2", 700, 200);
 
-
-
-        player2 = FXGL.entityBuilder()
-                .at(500, 200)
-                .viewWithBBox(texture("charlotte_zombie.png", 60, 80))
-                .with(new CollidableComponent(true))
-                .type(PlantsTypes.PLAYER)
-                .buildAndAttach();
-
-
-        plant1 = FXGL.entityBuilder()
-                .at(155,155)
-                .viewWithBBox(new Rectangle(30, 30, Color.YELLOW))
-                .with(new CollidableComponent(true))
-                .type(PlantsTypes.PLANT)
-                .buildAndAttach();
-
-        plant2 = FXGL.entityBuilder()
-                .at(155,235)
-                .viewWithBBox(new Rectangle(30, 30, Color.YELLOW))
-                .with(new CollidableComponent(true))
-                .type(PlantsTypes.PLANT)
-                .buildAndAttach();
+        plant1 = getGameWorld().spawn("plant", 155, 155);
+        plant2 = getGameWorld().spawn("plant", 155, 235);
 
         plants.add(plant1);
         plants.add(plant2);
 
-
         for (Entity plant : plants) {
+            int randomInterval = ThreadLocalRandom.current().nextInt(0,4) + 2;
             FXGL.getGameTimer().runAtInterval(() -> {
                 if (plant.isActive()) {
-                    erwt = FXGL.entityBuilder()
-                            .at(plant.getPosition())
-                            .viewWithBBox(new Rectangle(20, 20, Color.BROWN))
-                            .with(new CollidableComponent(true))
-                            .type(PlantsTypes.ERWT)
-                            .buildAndAttach();
-
-                    plantErwtMap.put(plant, erwt);
+                    erwten.add(getGameWorld().spawn("erwt", plant.getPosition()));
                 }
-            }, Duration.seconds(3));
-            if (!plant.isActive()) {
-                FXGL.getGameTimer().clear();
-            }
+            }, Duration.seconds(randomInterval));
         }
     }
 
     @Override
     protected void initPhysics() {
-        FXGL.getPhysicsWorld().addCollisionHandler(new CollisionHandler(PlantsTypes.PLAYER, PlantsTypes.ERWT) {
+        FXGL.getPhysicsWorld().addCollisionHandler(new CollisionHandler(EntityTypes.PLAYER, EntityTypes.ERWT) {
             @Override
             protected void onCollision(Entity player, Entity erwt) {
                 System.out.println("player removed");
                 player.removeFromWorld();
             }
         });
-        FXGL.getPhysicsWorld().addCollisionHandler(new CollisionHandler(PlantsTypes.PLAYER, PlantsTypes.PLANT) {
+        FXGL.getPhysicsWorld().addCollisionHandler(new CollisionHandler(EntityTypes.PLAYER, EntityTypes.PLANT) {
             @Override
             protected void onCollision(Entity player, Entity plant) {
                 System.out.println("plant removed");
@@ -150,45 +116,30 @@ public class PlantsApp extends GameApplication {
     @Override
     protected void initInput() {
         FXGL.onKey(KeyCode.D, () -> {
-            if (isPlayer1Active) {
-                player1.translateX(5);
-            } else {
-                player2.translateX(5);
-            }
+            (isPlayer1Active ? player1 : player2).translateX(5);
         });
         FXGL.onKey(KeyCode.A, () -> {
-            if (isPlayer1Active) {
-                player1.translateX(-5);
-            } else {
-                player2.translateX(-5);
-            }
+            (isPlayer1Active ? player1 : player2).translateX(-5);
         });
         FXGL.onKey(KeyCode.W, () -> {
-            if (isPlayer1Active) {
-                player1.translateY(-5);
-            } else {
-                player2.translateY(-5);
-            }
+            (isPlayer1Active ? player1 : player2).translateY(-5);
         });
         FXGL.onKey(KeyCode.S, () -> {
-            if (isPlayer1Active) {
-                player1.translateY(5);
-            } else {
-                player2.translateY(5);
-            }
+            (isPlayer1Active ? player1 : player2).translateY(5);
         });
     }
 
     @Override
     protected void onUpdate(double tpf) {
-        for (Map.Entry<Entity, Entity> entry : plantErwtMap.entrySet()) {
-            Entity plant = entry.getKey();
-            Entity erwt = entry.getValue();
-            if (!plant.isActive()) {
-                erwt.removeFromWorld();
-            }
+        List<Entity> removeErwten = new ArrayList<>();
+        for (Entity erwt : erwten) {
             erwt.translateX(5d);
+            if (erwt.getPosition().getX() > WIDTH_BOARD) {
+                erwt.removeFromWorld();
+                removeErwten.add(erwt);
+            }
         }
+        removeErwten.forEach(erwt -> erwten.remove(erwt));
     }
 
     public static void main(String[] args) {
--- demo/src/main/java/com/example/demo/EntityFactory.java
+++ demo/src/main/java/com/example/demo/EntityFactory.java
@@ -53,3 +53,4 @@ public class EntityFactory implements com.almasb.fxgl.entity.EntityFactory {
 
 
 }
+
